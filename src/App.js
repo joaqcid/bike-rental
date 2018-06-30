0a3654af86ef5d8b8fe4b61d7bb2e8a712fe394e
Joaquin Cid <joaqcid@gmail.com>
2018-06-30 16:59:59 -0300

    bike list

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import Layout from './components/Layout';
-import { Switch } from '@material-ui/core';
 import Home from './components/Home';
-import Bike from './components/bike-admin/Bike';
+import BikeAdmin from './components/bike-admin/BikeAdmin';
 
 class App extends React.Component {
   render() {
@@ -13,7 +12,7 @@ class App extends React.Component {
           <Layout />
           <Switch>
             <Route exact path="/" component={Home} />
-            <Route path="/bikes" component={Bike} />
+            <Route path="/bikes" component={BikeAdmin} />
           </Switch>
         </div>
       </BrowserRouter>
